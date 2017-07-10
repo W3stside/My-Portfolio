498bce4e3f4b7e13aaedf10bd1488f382769f4a8
David Sato <david.sato64@gmail.com>
2017-07-10 11:47:17 -0700

    changed jQuery to use better .queue // dequeue system + added changes in the About Me section

--- js/index.js
+++ js/index.js
@@ -9,9 +9,7 @@ $(document).ready(function() {
   const headerArr = $('.main-div');
   const animTime = 100;  
   const slidesHeight = $('.slides').innerHeight();
-  
-  //$('.slides').css({maxHeight: slidesHeight, overflow: 'hidden'});
-  
+    
   function clickExpander (e) {
     e.preventDefault;
     
@@ -19,35 +17,40 @@ $(document).ready(function() {
     const contentToOpenOnClick = '.' + this.id + '-content';
     const winWidth = $('html').innerWidth();
     const divWidth = $('h1', this).innerWidth() + 30;
-	const that = $(this);
-	
+	const that = this;
+		
 	//CHECK IF OPEN IF SO CLOSE
     if($(contentToOpenOnClick).hasClass('open')) {
 		$(contentToOpenOnClick).stop(true,true).toggleClass('hide')
-			.queue( () => {
-				$(contentToOpenOnClick).toggleClass('open')
-					.animate({}, 300, 
-						function nextAnimation () {
-							//wait before firing small font - take out of event loop till last
-							setTimeout(() => { that.siblings().toggleClass('small-font') }, 100);
-						})
-			});	
+		.queue( function () {
+			$(contentToOpenOnClick).toggleClass('open');
+			$(this).dequeue();
+		})
+		.queue( function () {
+			//wait before firing small font - take out of event loop till last
+			$(that).siblings().toggleClass('small-font');
+			$(this).dequeue();
+		})
+		.queue( function () {
+			
+			$(this).dequeue();
+		});
+		
 		//Stop function	
 		return;
 	}
 	
 	//OPENING ANIMATIONS
 	$(this).stop(true,true).siblings().toggleClass('small-font')
-		.queue( () => {
-			$(contentToOpenOnClick).toggleClass('open')
-				.queue(
-					setTimeout(
-						() => {
-							$(contentToOpenOnClick).toggleClass('hide');
-						}, 500
-					)
-				);//end queue 2	
-		});	//end queue 1	
+	
+	$(contentToOpenOnClick).queue( function () {
+		$(contentToOpenOnClick).toggleClass('open');
+		$(this).dequeue();
+	});	
+	$(contentToOpenOnClick).queue( function () {
+		$(contentToOpenOnClick).toggleClass('hide');
+		$(this).dequeue();
+	});	
   }	
   //bind each menu in the arr to the function clicker thangy thang thang
   $.each(headerArr, (index, val) => {
